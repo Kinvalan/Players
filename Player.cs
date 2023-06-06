30ca869798287d4923abf34e65db3f96da683197
Kinvalan <sebastianmoller01@icloud.com>
2023-06-06 22:50:26 +0200

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Player
+{
+    public string Name { get; private set; }
+    public int Points { get; private set; }
+
+    
+    public Player(string name, int points)
+    {
+        Name = name;
+        Points = points;
+    }
+
+    /*
+    Metoden Play() skal inneholde en måte å velge en tilfeldig vinner og taper av de som blir trukket. 
+    Vinneren skal få 1 poeng, taperen mister 1 poeng. Til slutt vises det hvor mange poeng hver av dem har.  
+    */
+    public void Play(Player opponent)
+    { 
+        var random = new Random();
+
+        double randomOutcome = random.NextDouble();
+
+
+        if(randomOutcome < 0.5)
+        {
+            // Den nåværende spilleren vinner
+            Points += 1;
+            opponent.Points -= 1;
+            Console.WriteLine($"{Name} vant mot {opponent.Name}.\n");
+        }
+
+        else if (randomOutcome > 0.5)
+        {
+            // Motstanderen vinner
+            Points -= 1;
+            opponent.Points += 1;
+            Console.WriteLine($"{opponent.Name} vant mot {Name}.\n");
+        }
+
+    }
+
+    public void ShowNameAndPoints()
+    {
+         Console.WriteLine($"{Name} har {Points} poeng.");
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+En spiller har et navn og et antall poeng. 
+Når to spiller mot hverandre, vil den ene miste et poeng og den andre få et poeng. 
+
+Koden under simulerer tre spillere med 10 poeng hver - og ti runder hvor to tilfeldige av dem spiller mot hverandre. 
+Lag klassen Player, slik at koden under fungerer:
+
+var random = new Random();
+var players = new[] { new Player("Per", 10), new Player("Pål", 10), new Player("Espen", 10) };
+for (var round = 1; round <= 10; round++)
+{
+    var playerIndex1 = random.Next(players.Length);
+    var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+    var player1 = players[playerIndex1];
+    var player2 = players[playerIndex2];
+    player1.Play(player2);
+}
+
+foreach (var player in players)
+{
+    player.ShowNameAndPoints();
+}
+*/
+
+
+using System.Numerics;
+
+namespace Players
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Playing();
+        }
+
+        public static void Playing()
+        {
+            var random = new Random();
+
+            var players = new[]
+                { 
+                    new Player("Per", 10),
+                    new Player("Pål", 10),
+                    new Player("Espen", 10)
+                 };
+
+            for(var round = 1; round <= 10; round++)
+            {
+                var playerIndex1 = random.Next(players.Length);
+                var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+
+                var player1 = players[playerIndex1];
+                var player2 = players[playerIndex2];
+
+                player1.Play(player2);
+            }
+
+            // Funksjonalitet for å bestemme hvem som vant totalt
+
+            Player winner = null!;
+            var maxPoints = int.MinValue;
+
+            foreach(var player in players)
+            {
+                player.ShowNameAndPoints();
+
+                if (player.Points > maxPoints)
+                {
+                    maxPoints = player.Points;
+                    winner = player;
+                }
+            }
+
+            if (winner != null)
+            {
+                Console.WriteLine($"\nVinneren av turneringen er {winner.Name} med {winner.Points} poeng!");
+            }
+
+        }
+    }
+}
